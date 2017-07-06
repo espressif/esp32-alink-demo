7240f81d0a33e0e6ce18da1fc2be4f7ee94cd5e1
zhanzhaochen <zhanzhaochen@espressif.com>
2017-07-06 14:18:28 +0800

    feat: Add logs save function in the script

--- gen_misc.sh
+++ gen_misc.sh
@@ -7,25 +7,43 @@ echo -e "\033[32m----------------------"
 echo "SDK_PATH: $IDF_PATH"
 echo -e "----------------------\033[00m"
 
-make -j8 flash
-
-# copy bin
 if [ ! -d "bin" ]; then
   mkdir bin
 fi
+
+if [ ! -d "log" ]; then
+  mkdir log
+fi
+
+# make clean
+make -j8
+xtensa-esp32-elf-objdump -S ./build/alink.elf > bin/alink.s &
+
+# copy bin
 echo -e "\033[32m----------------------"
 echo "the esptool.py copy to bin "
 echo "the alink.bin copy to bin "
 echo "the bootloader_bin copy to bin"
 echo "the partitions_two_ota.bin copy to bin"
-echo "the alink.s copy to bin"
+echo "the alink.elf copy to bin"
 echo -e "----------------------\033[00m"
 cp esp-idf/components/esptool_py/esptool/esptool.py bin
-cp ./build/alink.bin bin/
 cp ./build/bootloader/bootloader.bin bin
 cp  build/partitions_two_ota.bin bin
+cp ./build/*.bin bin/
+cp  build/*.elf bin
+cp  build/*.map bin
 
-xtensa-esp32-elf-objdump -S ./build/alink.elf > bin/alink.s &
+if [ ! -n "$1" ] ;then
+    log_file_name="log/"$(date "+%Y-%m-%d_%H-%M-%S")".log"
+    make erase_flash flash monitor | tee $log_file_name
+else
+    cd bin
+    python esptool.py --chip esp32 --port /dev/ttyUSB$1 erase_flash
+    python esptool.py --chip esp32 --port /dev/ttyUSB$1 --baud 1152000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 bootloader.bin 0x10000 *alink*.bin 0x8000 partitions_two_ota.bin
+    cd -
 
-make monitor
-
+    log_file_name="log/ttyUSB$1_"$(date "+%Y-%m-%d_%H-%M-%S")".log"
+    # pkill minicom
+    minicom -D /dev/ttyUSB$1 -c on -C "$log_file_name"
+fi
--- setup_toolchain.sh
+++ setup_toolchain.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-# 安装软件
-# sudo apt-get install git wget make libncurses-dev flex bison gperf python python-serial 
+# install software
+sudo apt-get install git wget make libncurses-dev flex bison gperf python python-serial
 
-# 下载工具链
+# Download the toolchain
 mkdir -p ~/esp
 cd ~/esp
 wget https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-61-gab8375a-5.2.0.tar.gz
 tar -xzf xtensa-esp32-elf-linux64-1.22.0-61-gab8375a-5.2.0.tar.gz
 
-# 配置环境变量
+# Configure environment variables
 echo 'export PATH=$PATH:$HOME/esp/xtensa-esp32-elf/bin' >> ~/.profile
 source ~/.profile
 
